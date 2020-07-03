99cd2cdca00149ba39b46d9aefb08587b0bd8c28
Raj <vishnu@dooziesoft.com>
2020-07-03 17:08:38 +0530

    newone

---
+++ mail/quote.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="keywords" content="Conceptly, Responsive, Business Template, Bootstrap 4,">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="The team at SyZyGy  will provide solutions to assist clients in managing and mitigating structural risk in large infrastructure projects." />
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon" />
+    <title>SyZyGy - Contact Us Page</title>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+ <link rel="stylesheet" href="assets/css/all.css">
+ <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+   <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
+
+<body>
+
+
+    <!-- Start: Preloader
+    ============================= -->
+
+    <div class="preloader">
+        <div class="loader">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <!-- End: Preloader
+    ============================= -->
+
+    <!-- Start: Header Top
+    ============================= -->
+    <section id="header-top">
+        <div class="container">
+            
+              <div class="row">
+                <div class="col-lg-9 col-md-12 text-lg-left mb-2 mb-lg-0">
+                    <ul class="header-info d-inline-block" style="margin-right: -20px;">
+                        <li><a href="#"><i class="fas fa-phone"></i>+91 98 806 49 128</a></li>
+                        <li><a href="#"><i class="fas fa-envelope"></i>madhan.bs@syzygystructures.com
+</a></li>
+                     
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-12 text-center text-lg-right mb-lg-0 mb-1">
+                    <ul class="header-social d-inline-block">
+                           <li><a href="https://www.facebook.com/Syzygy-Structural-Consultant-121993732532928" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://twitter.com/syzygyconsulta1" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/syzygy-structural-consultant-204525194/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                     
+                    </ul>                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End: Header Top
+    ============================= -->
+
+    <!-- Start: Search
+    ============================= -->
+
+    <div id="search">
+        <a href="#" id="close-btn">
+        <i class="fa fa-times"></i>
+        </a>
+        <div>
+            <input placeholder="type here" id="searchbox" type="search" />
+            <button type="submit" class="search-submit"><i class="fa fa-search"></i></button>
+        </div>
+    </div>
+
+    <!-- End: Search
+    ============================= -->
+
+    <!-- Start: Navigation
+    ============================= -->
+    <section class="navbar-wrapper">
+        <div class="navbar-area sticky-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-6">
+                        <div class="logo main">
+                         
+  <a href="index.html"><img class="responsive" src="assets/img/syzygyp.png" alt="Startkit"></a>
+                    
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-md-10 d-none d-lg-block text-right" style="margin-right: 17px;">
+                        <nav class="main-menu" style="margin-right: 190px;">
+                            <ul>
+                                <li>
+                                    <a href="index.html">Home</a>
+                                </li>
+                                   <li>
+                                    <a href="about-us.html">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="service.html">Services</a>
+                                </li>
+                                <li>
+                                    <a href="project.html">Project</a>
+                                </li>
+                            
+ <li><a href="gallery.html">Gallery</a></li>
+                                <li class="active"><a href="contact-us.html">Contact</a></li>
+                           
+                            </ul>
+
+                        </nav>
+                     
+                    
+                   
+                    </div>
+
+
+
+
+
+
+<div class="header-right-bar d-lg-inline-block d-none">
+    &nbsp;&nbsp;
+ <a class="quote-btn boxed-btn" href="contact-us.html">Get In Touch</a>
+</div>
+
+                </div>
+            </div>
+            <!-- Start Mobile Menu -->
+            <div class="mobile-menu-area d-lg-none">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="mobile-menu">
+                                <nav class="mobile-menu-active">
+                                    <ul>
+                                        <li>
+                                            <a href="index.html">Home</a>
+                                        </li>
+                                         <li class="active">
+                                            <a href="about-us.html">About Us</a>
+                                        </li>
+                                        <li>
+                                            <a href="service.html">Services</a>
+                                        </li>
+                                     
+                                        <li><a href="project.html">Project</a></li>
+                                       <li><a href="gallery.html">Gallery</a></li>   <li class="active"><a href="contact-us.html">Contact</a></li>
+                                    </ul>
+                                </nav>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Mobile Menu -->
+        </div>        
+    </section>
+    <!-- End: Navigation
+    ============================= -->
+
+
+    <!-- Start: Breadcrumb Area
+    ============================= -->
+
+    <section id="breadcrumb-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Contact Us</h1>
+                   
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- End: Breadcrumb Area
+    ============================= -->
+
+
+    <!-- Start: Contact Info
+    ============================= -->
+    
+    <section id="contact-info" class="section-padding-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 mb-md-0 mb-5 single-box text-center">
+                    <img src="assets/img/icons/address.png" alt="">
+                    <h3>Address</h3>
+                    <p>Structural Design Engineer SyZyGy Consultants
+       #155,60 feet Road Bank officers co-operative
+       society limited Narayan Nagar,1st block 
+       Doddakallasandra, kanakapura road
+       <br>Bangalore -62 </p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 mb-md-0 mb-5 single-box text-center">
+                    <img src="assets/img/icons/mail.png" alt="">
+                    <h3>E-mail</h3>
+                    <p>madhan.bs@syzygystructures.com  <br>syzygyconsultant@gmail.com</p>
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-6 single-box text-center">
+                    <img src="assets/img/icons/phone.png" alt="">
+                    <h3>Phone</h3>
+                    <p>+91 988 064 9128</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- End: Contact Info
+    ============================= -->
+
+    <!-- Start: Contact Info
+    ============================= -->
+    
+    <section id="send-message">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 order-2 order-md-2">
+                    <div id="map" style="margin-left: 70px;">
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.121514915111!2d77.57066281482123!3d12.899907090902936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1588aa958c0b%3A0x22edec2a6a4bb924!2sSyZyGy%20Structural%20Consultant!5e0!3m2!1sen!2sin!4v1570711801511!5m2!1sen!2sin" width="80%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 order-md-2 order-sm-1 contact-us-form">
+                    
+                 
+                    <h3>Send Us a Message</h3>
+                    <div class="g-recaptcha" data-sitekey="6LcPtBwUAAAAAF8HSAQAiy7lbqeoC8b-ErNP0fQe"></div>
+                    <div class="row">
+                        <div class="col-lg-6 form-group">
+
+                            <input type="text" class="form-control" placeholder="Your Name">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <input type="email" class="form-control" placeholder="Your Email">
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <input type="text" class="form-control" placeholder="Phone" required>
+                        </div>
+                        <div class="col-lg-6 form-group">
+                            <input type="email" class="form-control" placeholder="Subject">
+                        </div>
+                        <div class="col-sm-12 form-group">
+                            <textarea class="form-control" rows="3" placeholder="Your Mesage"></textarea>
+                        </div>
+                        <button class="boxed-btn">Send Us a Message</button>
+                    </div>
+                
+                
+
+                
+                
+                
+                
+                
+                 </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- End: Contact Info
+    ============================= -->
+
+    <!-- Start: Call to action
+    ============================= -->
+
+    <!-- Start: Footer Sidebar
+    ============================= -->
+      <footer id="footer-widgets">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12 mb-lg-0 mb-4">
+                    <aside class="widget widget_about">
+                        <div class="footer-logo">
+                     
+<h1 style="color: #ff5d00;"><b>SyZyGy</b></h1>
+                        </div>
+                       <p>At SyZyGy, we provide solutions and assist you in managing and mitigating structural risk in large infrastructure projects. This ultimately contributes to enhanced project performance through improved overall quality and durability and helps in on time project delivery and the minimised re-work.</p>
+                        <ul class="widget-social">
+                               <li><a href="https://www.facebook.com/Syzygy-Structural-Consultant-121993732532928" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                        <li><a href="https://twitter.com/syzygyconsulta1" target="_blank"><i class="fab fa-twitter"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/syzygy-structural-consultant-204525194/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                       
+                        </ul>
+                        
+                    </aside>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 mb-lg-0 mb-4">
+                    <aside class="widget widget_links">
+                        <h4 class="widget-title">Useful Links</h4>
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="about-us.html">About Us</a></li>
+                            <li><a href="service.html">Services</a></li>
+                            <li><a href="projectcomingsoon.html">Project</a></li>
+                         
+                            <li><a href="contact-us.html">Contact Us</a></li>
+                        </ul>
+                    </aside>
+                </div>
+         
+                <div class="col-lg-5 col-md-6 col-sm-12">
+                    <aside class="widget widget_contact">
+                        <h4 class="widget-title">Contact information</h4>
+                        <ul>
+                            <li><i class="fas fa-map-marker"></i>Structural Design Engineer SyZyGy Consultants<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+#155,60 feet Road Bank officers co-operative<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; society limited Narayan Nagar,1st block  <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Doddakallasandra, kanakapura road<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Bangalore -62
+</li>
+                            <li><i class="fas fa-envelope"></i>madhan.bs @syzygystructures.com
+ <br> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;syzygyconsultant@gmail.com</li>
+                            <li><i class="fas fa-phone"></i>+91 988 064 9128 <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </li>
+                        </ul>
+                      
+                    </aside>
+                </div>
+            </div>
+
+        </div>
+    </footer>
+    <!-- End: Footer Sidebar
+    ============================= -->
+
+    <!-- Start: Footer Copyright
+    ============================= -->
+
+    <section id="footer-copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-12 text-lg-left text-center mb-lg-0 mb-3 copyright-text">
+                      &copy;<script>new Date().getFullYear()>2010&&document.write(""+new Date().getFullYear());</script>, Powered by SyZyGy. Developed by <a href="https://dooziesoft.com" target="_blank">DoozieSoft</a>
+
+                </div>
+                <div class="col-lg-6 col-12">
+               
+                    <a href="#" class="scrollup boxed-btn"><i class="fas fa-arrow-up"></i></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- End: Footer Copyright
+    ============================= -->
+    
+    <!-- Scripts -->
+    <script src="assets/js/jquery-3.2.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.sticky.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.shuffle.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/jquery.meanmenu.min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.hoverdir.js"></script>
+
+    <!-- Smooth Scroll -->
+    <script src="assets/js/TweenMax.min.js"></script>
+    <script src="assets/js/ScrollToPlugin.min.js"></script>
+
+    <!-- Custom Script -->
+    <script src="assets/js/custom.js"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-148900867-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-148900867-1');
+</script>
+</body>
+
+
+
+</html>
